2a280edb6682de9cc124faeaedbdfe07bc75919c
coco <cocodelacueva@gmail.com>
2023-02-24 09:23:02 -0300

    listo para hacer llamada y hacerlo funcionar

--- src/views/public/js/pitutis.js
+++ src/views/public/js/pitutis.js
@@ -5,9 +5,10 @@ document.addEventListener('DOMContentLoaded', init, false);
 
 function init() {
     
-    let deleteSessionBTN = document.querySelector('#delrobot');
-    let startSessionBTN = document.querySelector('#startbtn');
-
+    const deleteSessionBTN = document.querySelector('#delrobot');
+    const startSessionBTN = document.querySelector('#startbtn');
+    const infoBox = document.querySelector('#infobox');
+    
     if ( startSessionBTN ) {
         startSessionBTN.addEventListener('click', (event)=>{
             fetch( urlAPI+'/start' )
@@ -37,8 +38,86 @@ function init() {
             .catch(err => console.log(err));
         })    
     }
+
+
+
+    //info instrucciones
+    if (infoBox) {
+        const toggleBtnInfo = document.querySelector('#toggle-info');
+
+        toggleBtnInfo.addEventListener('click', (event)=>{
+            if ( infoBox.classList.contains('on') ) {
+                infoBox.classList.remove('on');
+            } else {
+                infoBox.classList.add('on');
+            }
+        });
+
+        const closeBtnInfo = infoBox.querySelector('#close-info');
+        closeBtnInfo.addEventListener('click', (event) => {
+            infoBox.classList.remove('on');
+        })
+    }
+
+    //In init, must show info:
+    infoBox.classList.add('on');
+
+
+    //load robot functions
+
+    //talker
+    
+    const talkerBtn = document.querySelector('#talk-action-btn');
+    const actionsSelector = document.querySelector('#actions');
+
+    if ( talkerBtn ) {
+        talkerBtn.addEventListener('click', (event) => {
+            const talker = document.querySelector('#talker');
+            const msj = talker.value.trim();
+            if (msj == '') {
+                showHelpMessaje(['You need to write something.']);
+            }
+
+            //TODO
+            //hacer llamada a api
+        });
+    }
+
+    if ( actionsSelector ) {
+        actionsSelector.addEventListener('change', (event)=> {
+            const action = actionsSelector.value;
+            
+            if (action == 'none') {
+                showHelpMessaje(['You need to select something.']);
+            }
+
+            //TODO
+            //hacer llamada a api
+            
+        });
+    }
+    
+
 }
 
 function reload() {
     window.location.reload();
+}
+
+//array de msj
+function showHelpMessaje(msjs) {
+    const helpersMSJ = document.querySelector('#helpersmsj');
+    const container = helpersMSJ.querySelector('.msjhelperwrapper');
+    let html = '';
+
+    msjs.forEach(msj => {
+        html = html + '<p>' + msj + '</p>';
+    });
+
+    container.innerHTML = html;
+    
+    if (!helpersMSJ.classList.contains('on')) {
+        helpersMSJ.classList.add('on');
+    }
+    
 }
